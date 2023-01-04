476d007fb29df5d1e0e401724ba34e0ee1cb60e8
Ngetsophun <phunofficial18@gmail.com>
2023-01-05 00:33:26 +0700

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:delivery_food/pages/menuPage.dart';
+import 'package:delivery_food/Home/main_food_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MenuPage(),
+      home: MainFoodPage(),
       
      
     );
---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+class BigText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+  BigText({super.key, 
+  required this.color, 
+  required this.text,
+  this.size = 20,
+  this.overFlow= TextOverflow.ellipsis,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overFlow,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
